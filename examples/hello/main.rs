1c5e55d80db0af9fff9fa9964b10ecca191891cd
flevin58 <flevin58@gmail.com>
2024-09-06 14:20:05 +0200

    Initial commit. Created basic structure

---
+++ examples/hello/main.rs
@@ -0,0 +1,7 @@
+//!
+//! A simple demonstration of console output.
+//! It' the basic Hello World app :)
+//!
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+//!
+//! You shouldn't run this main program.
+//! Instead run an exmple as follows:
+//! cargo run --example example_name
+//!
+fn main() {
+    println!("To run one of the examples please run:");
+    println!("cargo run --example example_name");
+}
